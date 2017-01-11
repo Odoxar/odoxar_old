accab10206f4c9b802f8b0cca82cd31f4b07ae05
Denys <odoxar@gmail.com>
2017-01-11 19:10:17 +0200

    contactForm

---
+++ ajax-forma/mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "odoxar@gmail.com"; /*Укажите адрес, га который должно приходить письмо*/
+    $sendfrom   = "smart-landing@yandex.ru"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData<br> <b>Имя пославшего:</b> $name <br><b>Телефон:</b> $phone";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<center><p class="success">Спасибо за отправку вашего сообщения!</p></center>';
+    }
+    else 
+    {
+    echo '<center><p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p></center>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
